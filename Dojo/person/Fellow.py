1a98cc1fbc123c7795571876894c02107e028e37
Eunice <euchiri@gmail.com>
2017-11-09 08:22:52 +0000

    changes on the fellow and requirements file

--- Dojo/person/Fellow.py
+++ Dojo/person/Fellow.py
@@ -2,7 +2,7 @@ import Person
 
 class Fellow(Person):
 
-    def __init__(self,first_name,last_name,fellow_id,lang):
+    def __init__(self,first_name,last_name,fellow_id):
         super(Fellow,self).__init__(first_name,last_name)
         self.fellow_id = fellow_id
-        self.lang = lang
+       
