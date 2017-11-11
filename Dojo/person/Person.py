4800a00f885247cb60ceb5d8d37a5ef6c76cc3aa
eunicejk12 <euchiri@gmail.com>
2017-11-11 13:20:20 +0300

    add major changes

--- Dojo/person/Person.py
+++ Dojo/person/Person.py
@@ -15,6 +15,6 @@ class Person(object):
 
 class Staff(Person):
     '''staff class for the staff employed'''
-    def __init__(self, first_name, last_name, staff_id):
+    def __init__(self, first_name, last_name):
         super(Staff, self).__init__(first_name, last_name)
-        self.staff_id = staff_id
+       
